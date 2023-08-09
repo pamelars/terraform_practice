ddd1342acf194bdc01f54e761d90f12c12abbe19
Pamela Moore <pamelamoore@PamelaMesiMac53.verizon.net>
2023-08-08 22:19:50 -0400

    got rid of password

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4OSPHAFMI5JT66YB"
-  secret_key = "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"
 }
 
 resource "aws_instance" "pamsec2" {
